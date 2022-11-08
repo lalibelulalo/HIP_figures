6069c9755939e21475921df3a1b28e28688a3781
lalibelulalo <62567731+lalibelulalo@users.noreply.github.com>
2022-11-07 23:25:46 -0600

    Update Count.py

--- Count.py
+++ Count.py
@@ -2,12 +2,12 @@
 ## python Conteo.py genomes_dir KMERS MODEL
 ## Where:
 ## KMERS: 
-##	8. Octamers
-##	6. Hexamers
-##  5. Pentamers
-##  4. Tetramers
-##  61. HexamersNoHIP
-##	41. TetramersNoHIP
+## 8. Octamers
+## 6. Hexamers
+## 5. Pentamers
+## 4. Tetramers
+## 61. HexamersNoHIP
+## 41. TetramersNoHIP
 ## MODEL: 0,1,2,3
 ## python Conteo.py all_pico_2022_fna 41 2
 import sys
